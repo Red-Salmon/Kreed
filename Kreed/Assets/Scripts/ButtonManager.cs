0b9788e5fd50c1042d1f946326609dd0a43796b9
Emad <sheikhemaduddin@gmail.com>
2019-08-03 19:45:22 +0500

    Added a few new scripts
    We need a way to transfer data about which abilities were selected in the AbilitySelectScene to the BattleScene. Was trying out a way to achieve that.

---
+++ Kreed/Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour
+{
+    public void LoadLevel()
+    {
+        //public TMPro.TextMeshProUGUI abil1;
+
+        //GlobalVariables.abil1 = abil1.text;
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
