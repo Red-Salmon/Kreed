38cf73fc2eacc715b8601554bea4da9ed680892a
Kraily4t8 <tomtien2002@outlook.com>
2019-08-03 13:29:12 -0700

    Main Menu
    Includes responsive buttons
    such as a functional 'play' button (will need adjustments, as rn it goes to the next scene), a options menu (doesn't actually adjust volume as of the moment), and a functional exit button (doesn't actually work while in unity developer)

---
+++ Kreed/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame ()
+    {
+        //goes to the next level in the scene manager (so rn it goes to the abilities select)
+        //NEEDS TO BE ADJUST LATER!! so it will go to the correct scene right after the menu
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("QUIT!");
+        Application.Quit();
+    }
+}
