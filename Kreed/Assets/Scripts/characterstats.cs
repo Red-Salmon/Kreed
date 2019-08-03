ffabbc3717a603e3e57225fbfc7f3856606e4efd
Emad <sheikhemaduddin@gmail.com>
2019-08-03 18:54:47 +0500

    Added a stats script and a character prefab

---
+++ Kreed/Assets/Scripts/characterstats.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class characterstats : MonoBehaviour
+{
+
+    public float maxHealth;
+    public float currentHealth;
+
+    public float percentResist;
+    public float percentBuff;
+
+    public TMPro.TextMeshProUGUI display;
+
+    void Awake()
+    {
+        currentHealth = maxHealth;
+        percentBuff = 0;
+    }
+
+    
+    void Update()
+    {
+        display.text = currentHealth + "/" + maxHealth;
+    }
+
+    //This procedure will check if the character is still alive
+    public bool IsAlive()
+    {
+        if (currentHealth <= 0)
+        {
+            return false;
+        } else
+        {
+            return true;
+        }
+    }
+
+    // We will not reduce the health of the character directly.
+    // This method will be used so that we dont have to check the resistance every time
+    public void TakePhyDamage(float damage)
+    {
+        currentHealth -= ((100 - percentResist)/100) * damage;
+        if (currentHealth < 0) { currentHealth = 0; }
+    }
+
+    
+    //public void HealUp(float heal)
+    //{
+    //    if (currentHealth > 0 & currentHealth < maxHealth)
+    //    {
+    //        currentHealth += heal;
+    //        if (currentHealth > maxHealth) { currentHealth = maxHealth; }
+    //    }
+    //}
+}
