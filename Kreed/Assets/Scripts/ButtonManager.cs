5bedbe58627404f8ca15cfd5b80c4d629a813125
Emad <sheikhemaduddin@gmail.com>
2019-08-04 16:53:12 +0500

    Ability description is shown on screen

--- Kreed/Assets/Scripts/ButtonManager.cs
+++ Kreed/Assets/Scripts/ButtonManager.cs
@@ -7,6 +7,7 @@ using UnityEngine.UI;
 public class ButtonManager : MonoBehaviour
 {
     public int totalAbil;
+    public TMPro.TextMeshProUGUI abilityDetails;
     public Button startLevel;
 
     public void Awake()
@@ -31,6 +32,58 @@ public class ButtonManager : MonoBehaviour
         if (totalAbil >= 5)
         {
             startLevel.interactable = true;
+        }
+    }
+
+    public void ShowAbil(int x)
+    {
+        switch (x)
+        {
+            case 1:
+                abilityDetails.text = "Deals 10 fire damage";
+                break;
+            case 2:
+                abilityDetails.text = "Deal 10 fire damage, if enemy was attacked with fire damage, double this damage";
+                break;
+            case 3:
+                abilityDetails.text = "Deals 30 fire damage";
+                break;
+            case 4:
+                abilityDetails.text = "Deal 10 water damage, enemy takes 50% more damage from fire damage for 1 turn ";
+                break;
+            case 5:
+                abilityDetails.text = "Deal 15 water damage, if enemy was attacked with fire damage, triple this damage";
+                break;
+            case 6:
+                abilityDetails.text = "Deal 50 water damge";
+                break;
+            case 7:
+                abilityDetails.text = "The enemy has been tagged as cursed";
+                break;
+            case 8:
+                abilityDetails.text = "Half the HP of a cursed enemy";
+                break;
+            case 9:
+                abilityDetails.text = "One third the HP of a cursed enemy";
+                break;
+            case 10:
+                abilityDetails.text = "Nullify all damage";
+                break;
+            case 11:
+                abilityDetails.text = "Half the damage taken";
+                break;
+            case 12:
+                abilityDetails.text = "Reflect the damage taken back to the monster";
+                break;
+            case 13:
+                abilityDetails.text = "Gives you +100 health before battle";
+                break;
+            case 14:
+                abilityDetails.text = "All your attacks deal double damage";
+                break;
+            default:
+                abilityDetails.text = "";
+                break;
         }
     }
 }
