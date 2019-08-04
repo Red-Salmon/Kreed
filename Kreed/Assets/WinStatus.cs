705e2a64717d37a552b0dbe23d0ad86c67d27fd9
Kraily4t8 <tomtien2002@outlook.com>
2019-08-04 11:08:54 -0700

    VictoryText

---
+++ Kreed/Assets/WinStatus.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class WinStatus : MonoBehaviour
+{
+    public bool FinalStatus;
+    public bool VictoryStatus;
+    public GameObject VictoryPanel;
+    public TextMeshProUGUI VictoryText;
+
+    void FixedUpdate()
+    {
+        if (FinalStatus)
+        {
+            VictoryText.GetComponent<TextMeshProUGUI>();
+            if (VictoryStatus)
+            {
+                victory();
+            }
+            else
+            {
+                defeat();
+            }
+        }
+    }
+
+    void victory()
+    {
+        VictoryPanel.SetActive(true);
+        TextMeshPro VictoryText = GetComponent<TextMeshPro>();
+        VictoryText.SetText("Victory!");
+    }
+    
+    void defeat()
+    {
+        VictoryPanel.SetActive(true);
+        TextMeshPro VictoryText = GetComponent<TextMeshPro>();
+        VictoryText.SetText("Defeat!");
+    }
+}
