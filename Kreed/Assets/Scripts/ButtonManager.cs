b38e4a5168da1456aa636650454e609c05e45fdb
Emad <sheikhemaduddin@gmail.com>
2019-08-04 23:06:06 +0500

    Added Lacha's BG music

--- Kreed/Assets/Scripts/ButtonManager.cs
+++ Kreed/Assets/Scripts/ButtonManager.cs
@@ -35,7 +35,7 @@ public class ButtonManager : MonoBehaviour
         {
             totalAbil++;
             abilityCount.text = totalAbil.ToString() + "/5";
-
+            
             switch (totalAbil)
             {
                 case 1:
